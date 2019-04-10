f53ca83908d05116179add554959132e2000ccd8
tnicolas42 <tnicolas@student.42.fr>
2019-04-10 11:23:23 +0200

    add mean_time file

--- get_time.sh
+++ get_time.sh
@@ -1,6 +1,10 @@
 #!/bin/zsh
 
-result=`(time python3 npuzzle.py $1)  2>&1 >/dev/null | grep "python" | awk '{print $4}'`
+npuzzle='npuzzle.py'
+if [[ "$2" != "" ]]; then
+    npuzzle="$2/$npuzzle"
+fi
+result=`(time python3 $npuzzle $1)  2>&1 >/dev/null | grep "python" | awk '{print $4}' | rev | cut -c 2- | rev`
 echo $result
 
 exit 0
