5c1a6c07bd5e749bdb8d0691036668f367ee33a6
zer0nim <emarin@e2r13p8.42.fr>
2019-04-11 18:57:13 +0200

    Center and focus windows

--- srcs/gui/gui.py
+++ srcs/gui/gui.py
@@ -4,6 +4,8 @@ import PIL.Image, PIL.ImageTk
 import srcs.global_var as g
 from srcs.gui.utility import Point
 from srcs.generate_puzzle import spiral
+import os
+from platform import system as platform
 
 class npuzzleGui:
     """
@@ -13,8 +15,10 @@ class npuzzleGui:
 
     def __init__(self, win, win_title, img_path, puzzle):
         self.win = win
-        self.puzzle = puzzle
         self.win.title(win_title)
+        self.win.resizable(0, 0) # Don't allow resizing in the x or y direction
+
+        self.puzzle = puzzle
 
         # Load an image using OpenCV
         self.cv_img = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
@@ -26,16 +30,32 @@ class npuzzleGui:
 
         # calculate the box width
         self.box_w = int(self.img_w / self.puzzle.size)
-
         self.get_boxes_img()
+        self.printPuzzle()
 
+
+        self.centerWindows()
+        if platform() == 'Darwin':  # How Mac OS X is identified by Python
+            os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' ''')
+        self.win.mainloop()
+
+    def centerWindows(self):
+        """
+        "center" the windows in the middle of the screen
+        """
+        # Gets both half the screen width/height and window width/height
+        positionRight = int(self.win.winfo_screenwidth()/2 - self.img_w/2)
+        positionDown = int(self.win.winfo_screenheight()/2 - self.img_h/2)
+
+        # Positions the window in the center of the page.
+        self.win.geometry("+{}+{}".format(positionRight, positionDown))
+
+    def printPuzzle(self):
         i = 0
         for y in range(self.puzzle.size):
             for x in range(self.puzzle.size):
                 self.show_image(Point(x * self.box_w, y * self.box_w), list(g.resolved_puzzle).index(self.puzzle[i]))
                 i += 1
-
-        self.win.mainloop()
 
     def get_boxes_img(self):
         """
